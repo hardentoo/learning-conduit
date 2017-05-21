e380423dd1d4e25d5b23de6efd96ca714ef71443
Steven Shaw <steven@steshaw.org>
2017-05-21 18:18:12 +1000

    More complex example

--- more-complex.hs
+++ more-complex.hs
@@ -6,7 +6,12 @@ import Conduit
 main :: IO ()
 main = do
   putStrLn "List version:"
-  print $ take 10 [1..]
+  print $ takeWhile (< 18) $ map (* 2) $ take 10 [1..]
   putStrLn ""
   putStrLn "Conduit version:"
-  print $ runConduitPure $ yieldMany [1..] .| takeC 10 .| sinkList
+  print $ runConduitPure
+     $ yieldMany [1..]
+    .| takeC 10
+    .| mapC (* 2)
+    .| takeWhileC (< 18)
+    .| sinkList
