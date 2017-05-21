ced0dc4d0d0bf353d680333cc5a1f18e5c28febe
Steven Shaw <steven@steshaw.org>
2017-05-21 18:11:39 +1000

    Bad list example

---
+++ bad-list.hs
@@ -0,0 +1,14 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-8.12 --package conduit-combinators
+
+-- {-# LANGUAGE ExtendedDefaultRules #-}
+
+import Conduit
+
+main :: IO ()
+main = do
+  putStrLn "List version:"
+  print $ take 10 [1..]
+  putStrLn ""
+  putStrLn "Conduit version:"
+  print $ runConduitPure $ yieldMany [1..] .| takeC 10 .| sinkList
