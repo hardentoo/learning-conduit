a5d2e1d4c468758405c75b81020b79529b23e91d
Steven Shaw <steven@steshaw.org>
2017-06-13 19:04:48 +1000

    List versions of averaging

---
+++ lambdaconf-2017/12-average-list.hs
@@ -0,0 +1,35 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-8.12
+    --package conduit-combinators
+    --
+    -Wall -fwarn-tabs -fno-warn-type-defaults
+-}
+import Control.Arrow
+import Control.Monad
+import Data.Foldable
+import Data.Monoid
+import Data.Traversable
+
+-- Using foldMap.
+average1 :: [Double] -> Double
+average1 = foldMap (Sum &&& Sum . const 1) >>> join (***) getSum >>> uncurry (/)
+
+-- Using mapAccumL.
+average2 :: [Double] -> Double
+average2 ls = uncurry (/) $ fst $ mapAccumL f (0, 0) ls
+  where
+    f :: (Double, Double) -> Double -> ((Double, Double), Double)
+    f (total, len) b = ((total + b, len + 1), b)
+
+-- Using foldl' (don't need full power of mapAccumL).
+average3 :: [Double] -> Double
+average3 ls = uncurry (/) $ foldl' f (0, 0) ls
+  where
+    f :: (Double, Double) -> Double -> (Double, Double)
+    f (total, len) b = (total + b, len + 1)
+
+main :: IO ()
+main = do
+  let ns = [1 .. 10]
+  mapM_ (\f -> print $ f ns) [average1, average2, average3]
