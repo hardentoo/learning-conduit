d27fcee38d0979be8768a8686ea20064599a8cf3
Steven Shaw <steven@steshaw.org>
2017-06-13 19:44:31 +1000

    Use composition rather than >>>

--- lambdaconf-2017/12-average-list.hs
+++ lambdaconf-2017/12-average-list.hs
@@ -13,18 +13,18 @@ import Data.Traversable
 
 -- Using foldMap.
 average1 :: [Double] -> Double
-average1 = foldMap (Sum &&& Sum . const 1) >>> join (***) getSum >>> uncurry (/)
+average1 = uncurry (/) . join (***) getSum . foldMap (Sum &&& Sum . const 1)
 
 -- Using mapAccumL.
 average2 :: [Double] -> Double
-average2 ls = uncurry (/) $ fst $ mapAccumL f (0, 0) ls
+average2 = uncurry (/) . fst . mapAccumL f (0, 0)
   where
     f :: (Double, Double) -> Double -> ((Double, Double), Double)
     f (total, len) b = ((total + b, len + 1), b)
 
 -- Using foldl' (don't need full power of mapAccumL).
 average3 :: [Double] -> Double
-average3 ls = uncurry (/) $ foldl' f (0, 0) ls
+average3 = uncurry (/) . foldl' f (0, 0)
   where
     f :: (Double, Double) -> Double -> (Double, Double)
     f (total, len) b = (total + b, len + 1)
