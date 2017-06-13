52ac82e61ee1f51db4a5897a8b919c434abc3185
Steven Shaw <steven@steshaw.org>
2017-06-13 14:40:02 +1000

    Does not seem to require ExistentialQuanitification

--- lambdaconf-2017/04-loud-yield.hs
+++ lambdaconf-2017/04-loud-yield.hs
@@ -5,17 +5,18 @@
     --
     -Wall -fwarn-tabs -fno-warn-type-defaults
 -}
-{-# LANGUAGE ExistentialQuantification #-}
 
 import Conduit
 
-loudYield :: forall i. Int -> ConduitM i Int IO ()
+loudYield :: Int -> ConduitM i Int IO ()
 loudYield x = do
   liftIO $ putStrLn $ "yielding: " ++ show x
   yield x
 
-loudSinkNull :: forall o. ConduitM Int o IO ()
+loudSinkNull :: ConduitM Int o IO ()
 loudSinkNull = mapM_C $ \x -> putStrLn $ "awaited: " ++ show x
 
 main :: IO ()
-main = runConduit $ mapM_ loudYield [1 .. 3] .| loudSinkNull
+main = runConduit $
+  do { loudYield 1; loudYield 2; loudYield 3 } .|
+  loudSinkNull
