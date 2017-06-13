83be48dd611c1ba39edbe4642bdbe2b7cefd7d93
Steven Shaw <steven@steshaw.org>
2017-06-13 16:20:11 +1000

    Using undefined

---
+++ lambdaconf-2017/08-undefined-1.hs
@@ -0,0 +1,11 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-8.12
+    --package conduit-combinators
+    --
+    -Wall -fwarn-tabs -fno-warn-type-defaults
+-}
+import Conduit
+
+main :: IO ()
+main = runConduit $ undefined .| return ()
--- lambdaconf-2017/undefined-3.hs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env stack
--- stack --resolver lts-8.12 script --package conduit-combinators
-import Conduit
-
-main = runConduit $ return () .| undefined
